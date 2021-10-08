271a46efa1efd07cc1a29c8b064d6de3ba5be548
Jin Park <jinpark1504@gmail.com>
2021-10-07 21:03:29 -0400

    set caching assets

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // scope will be the location of sw.js
 // service worker only work on https connection, but localhost has exception
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
+    navigator.serviceWorker.register('/PWA-ninja/pwa-tutorial/sw.js')
         .then((reg) => console.log('service worker registered', reg))
         .catch((err) => console.log('service worker not registered', err))
 }
--- sw.js
+++ sw.js
@@ -1,6 +1,26 @@
+const staticCacheName = 'site-static';
+const assets = [
+    './',
+    './index.html',
+    './js/app.js',
+    './js/ui.js',
+    './js/materialize.min.js',
+    './css/styles.css',
+    './css/materialize.min.css',
+    './img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+];
 // install service worker
-self.addEventListener('install', () => {
-    console.log('service worker has been installed.');
+self.addEventListener('install', evt => {
+    //console.log('service worker has been installed.');
+    evt.waitUntil(
+        // create cache
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            // save cache
+            cache.addAll(assets);
+        })
+    )    
 });
 
 // activate service worker when the page is open
@@ -12,5 +32,12 @@ self.addEventListener('activate', () => {
 // fetch event - get source from the server
 // service worker will interrupt connection between a browser and a server
 self.addEventListener('fetch', evt => {
-    console.log('fetch event', evt);
+    //console.log('fetch event', evt);
+
+    // using cache
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    )
 });

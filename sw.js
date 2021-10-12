d11e5417dcbf27bb58ed95fbab83eb2ca050e9a3
Jin Park <jinpark1504@gmail.com>
2021-10-12 00:00:41 -0400

    add fallback page for offline

--- sw.js
+++ sw.js
@@ -1,16 +1,17 @@
 const staticCacheName = 'site-static-v2';
-const dynamicCache = 'site-dynamic-v1';
+const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
-  './',
-  './index.html',
-  './js/app.js',
-  './js/ui.js',
-  './js/materialize.min.js',
-  './css/styles.css',
-  './css/materialize.min.css',
-  './img/dish.png',
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
-  'https://fonts.gstatic.com/s/materialicons/v109/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2'
+  'https://fonts.gstatic.com/s/materialicons/v109/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
+  '/pages/fallback.html'
 ];
 // install service worker
 self.addEventListener('install', evt => {
@@ -34,7 +35,7 @@ self.addEventListener('activate', evt => {
       console.log(keys);
       // delete old key to change the cache
       return Promise.all(keys
-      .filter(key => key !== staticCacheName)
+      .filter(key => key !== staticCacheName && key !== dynamicCacheName)
       .map(key => caches.delete(key))
       );
     })
@@ -49,12 +50,14 @@ self.addEventListener('fetch', evt => {
   // using cache
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
+      // return cacheRes || fetch(evt.request);
+      // CREATE DYNAMIC CACHE WHEN A USER ENTER THE PAGE ON ONLINE
       return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCache).then(cache => {
+        return caches.open(dynamicCacheName).then(cache => {
           cache.put(evt.request.url, fetchRes.clone());
           return fetchRes;
         });
-      })
+      }).catch(() => caches.match('/pages/fallback.html'));
     })
   );
 });
